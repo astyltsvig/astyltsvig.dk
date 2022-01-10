568ea950f878feedcebf8c1ead51a8bcbbdedda7
Andreas Styltsvig <astyltsvig@gmail.com>
2022-01-10 20:36:15 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         <a href="mailto:astyltsvig@gmail.com">astyltsvig@gmail.com</a>
-<br/>
+        {"\n"}
         CVR: 36163771
       </footer>
     </>
