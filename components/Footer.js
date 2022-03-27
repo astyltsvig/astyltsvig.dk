9355878a99f19a00b65d8ceb42a145b5bec72b95
Andreas Styltsvig <astyltsvig@gmail.com>
2022-03-27 21:23:03 +0200

    Improve styling for footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,13 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        astyltsvig@gmail.com ~ CVR: 36163771
+        <div class="text">
+        <a href="mailto:astyltsvig@gmail.com">astyltsvig@gmail.com</a> | cvr 36163771
+        </div>
       </footer>
     </>
-  )
+  );
 }
